f5e056ab853021bcc2c9b8fb621c6936fc836ca5
Aarroan <aarroanpraveen@gmail.com>
2026-04-17 20:30:09 +0400

    server js updated

--- server/server.js
+++ server/server.js
@@ -28,8 +28,10 @@ function getHostname(origin) {
 }
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// FIX: Increased payload limit to 50mb to allow multiple image uploads at once!
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(
   cors({
     origin: (origin, callback) => {
